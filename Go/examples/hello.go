e6bce0121bc59dc6d2217944e371151193fb4341
Matthew Clark <mclark4386@gmail.com>
2018-01-09 00:57:45 -0600

    *post video version of hello.go example

--- Go/examples/hello.go
+++ Go/examples/hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	rand.Seed(time.Now().UnixNano())
+	fmt.Printf("random: %v\n", rand.Intn(100))
 }
