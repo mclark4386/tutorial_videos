dedb7843a6ca05e854e534e657ae958d17762db8
Matthew Clark <mclark4386@gmail.com>
2018-01-22 00:35:21 -0600

    *after video changes

--- Go/examples/bulls_cows.go
+++ Go/examples/bulls_cows.go
@@ -6,63 +6,66 @@ import (
 	"time"
 	"bufio"
 	"os"
-	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 var reader *bufio.Reader
+//                     0      1        2        3        4
+var words = []string{"cat", "dog", "gopher", "apple", "whale"}
+
+func init(){
+	fmt.Println("Want to play a game?")
+	rand.Seed(time.Now().UnixNano())
+	reader = bufio.NewReader(os.Stdin)
+}
 
 func main() {
-	initialize()
-	the_number := rand.Intn(100)
+	the_word := words[rand.Intn(len(words))]
 
+	fmt.Printf("Guess a word of length %d:", len(the_word))
+	
 	for {		
-		i, err := read_user_input()
+		str, err := read_user_input()
 		if err != nil {
-			if strings.Contains(fmt.Sprintf("%v",err),"strconv.Atoi") {
-				continue
-			}
 			return
 		}
 
-		if you_win(the_number, i) {
+		if you_win(the_word, strings.TrimSpace(str)) {
 			return
 		}
 	}
 }
 
 
-func you_win(the_number, i int) bool {
-		if i == the_number {
+func you_win(the_winner, guess string) bool {
+		if guess == the_winner {
 			fmt.Println("YOU WIN!")
 			return true
-		}else if i > the_number {
-			fmt.Println("Too High")
-			return false
 		}else{
-			fmt.Println("Too Low")
+			if len(the_winner) != len(guess) {
+				fmt.Printf("Guess a word of length %d:", len(the_winner))
+			}else{
+				bulls, cows := 0, 0
+				for i, c := range guess{
+					win_rune,_ := utf8.DecodeRuneInString(the_winner[i:])
+					if c == win_rune {
+						bulls += 1
+					}else if strings.ContainsRune(the_winner, c) {
+						cows += 1
+					}
+				}
+				fmt.Printf("Bulls:%d Cows:%d :",bulls, cows)
+			}
 			return false
 		}
 }
 
-func read_user_input() (int, error){
+func read_user_input() (string, error){
 	str, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Printf("ERROR!!!!: %v\n",err)
-		return 0, err
+		return "", err
 	}
-
-	i, err2 := strconv.Atoi(strings.TrimSpace(str))
-	if err2 != nil{
-		fmt.Println("Please only enter a number 1-100!")
-		return i, err2
-	}
-	return i, nil
-}
-
-func initialize(){
-	fmt.Println("Want to play a game?")
-	fmt.Println("Try to guess the number(1-100):")
-	rand.Seed(time.Now().UnixNano())
-	reader = bufio.NewReader(os.Stdin)
+	return str, nil
 }
